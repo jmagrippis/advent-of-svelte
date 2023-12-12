e30b12c75efdb3dc29be04e041d01996a99c0c64
Johnny Magrippis <j@magrippis.com>
2023-12-12 18:56:42 +0100

    ✨  implement merry munch-o-meter in SvelteKit

---
+++ apps/sveltekit/src/routes/munch-o-meter/Button.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	let className = ''
+
+	export {className as class}
+	export let value: string
+	export let disabled = false
+</script>
+
+<button
+	name="operation"
+	{disabled}
+	class={`${className} ${
+		disabled ? 'pointer-events-none' : ''
+	} bg-surface-brand-2 relative rounded-lg p-2 shadow transition duration-300 hover:brightness-125 active:-translate-y-1 disabled:opacity-50`}
+	{value}><slot /></button
+>
---
+++ apps/sveltekit/src/routes/munch-o-meter/Meter.svelte
@@ -0,0 +1,78 @@
+<script lang="ts">
+	import Cookie from '$lib/icons/Cookie.svelte'
+	import type {EventHandler} from 'svelte/elements'
+	import Button from './Button.svelte'
+	import {fly} from 'svelte/transition'
+	import {quintOut} from 'svelte/easing'
+
+	const operations = [
+		{label: '+ Add', value: 'increment'},
+		{label: '- Reduce', value: 'decrement'},
+		{label: '- Reset', value: 'reset'},
+	]
+
+	let count = 0
+
+	const handleSubmit: EventHandler<SubmitEvent, HTMLFormElement> = (event) => {
+		const operation = (event.submitter as HTMLButtonElement)?.value
+		switch (operation) {
+			case 'increment':
+				count++
+				break
+			case 'decrement':
+				count--
+				break
+			case 'reset':
+				count = 0
+				break
+			default:
+				break
+		}
+	}
+</script>
+
+<div class="grid grid-cols-2 place-items-center justify-center text-9xl">
+	<Cookie class="w-32" />
+	<div class="grid h-full w-full items-center justify-center">
+		{#key count}
+			<span
+				class="count"
+				transition:fly={{
+					delay: 250,
+					duration: 300,
+					y: 100,
+					easing: quintOut,
+				}}
+			>
+				{count}
+			</span>
+		{/key}
+	</div>
+</div>
+
+<form on:submit|preventDefault={handleSubmit} class="grid grid-cols-3 gap-4">
+	{#each operations as { label, value }}
+		<Button disabled={value === 'decrement' && count <= 0} {value}
+			>{label}</Button
+		>
+	{/each}
+</form>
+
+<section class="flex gap-4 text-xl">
+	<div>Mood 🎅:</div>
+	<div>
+		{#if count === 0}
+			hangry 😡
+		{:else if count > 10}
+			stuffed 😋
+		{:else}
+			jolly 😁
+		{/if}
+	</div>
+</section>
+
+<style>
+	.count {
+		grid-area: 1/1;
+	}
+</style>
