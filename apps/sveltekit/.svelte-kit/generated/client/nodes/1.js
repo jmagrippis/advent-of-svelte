5b3ea71215b09926348ed9704203aae3e040b823
Johnny Magrippis <j@magrippis.com>
2023-12-12 09:52:09 +0100

    🙈 that directory shouldn't be in version control

--- apps/sveltekit/.svelte-kit/generated/client/nodes/1.js
+++
@@ -1,1 +0,0 @@
-export { default as component } from "../../../../../../node_modules/.pnpm/@sveltejs+kit@1.28.0_svelte@4.2.8_vite@5.0.7/node_modules/@sveltejs/kit/src/runtime/components/error.svelte";
