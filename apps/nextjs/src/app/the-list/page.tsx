fa3bcd858e95bed60091fd31556c826c6bcb5d67
Johnny Magrippis <j@magrippis.com>
2023-12-12 16:57:12 +0100

    ✨  display "The List" in React app

--- apps/nextjs/src/app/the-list/page.tsx
+++ apps/nextjs/src/app/the-list/page.tsx
@@ -1,11 +1,24 @@
-import {NaughtyOrNiceTracker} from '@/components/NaughtyOrNiceTracker'
+import {NaughtyOrNiceTracker} from './NaughtyOrNiceTracker'
 
-const TheList = () => (
-	<main className="content-grid gap-y-4 lg:gap-y-8">
-		<h1 className="text-4xl font-bold lg:text-6xl">Naughty or Nice Tracker</h1>
+type Props = {
+	searchParams?: {
+		query?: string
+		status?: string
+	}
+}
+const TheList = ({searchParams}: Props) => {
+	const query = searchParams?.query
+	const status = searchParams?.status
 
-		<NaughtyOrNiceTracker />
-	</main>
-)
+	return (
+		<main className="content-grid gap-y-4 lg:gap-y-8">
+			<h1 className="text-4xl font-bold lg:text-6xl">
+				Naughty or Nice Tracker
+			</h1>
+
+			<NaughtyOrNiceTracker query={query} status={status} />
+		</main>
+	)
+}
 
 export default TheList
