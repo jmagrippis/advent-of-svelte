f20490063864e6aeb01d241f187c780cd421bc47
Johnny Magrippis <j@magrippis.com>
2023-12-12 17:36:22 +0100

    ✨  implement SvelteKit version

---
+++ apps/sveltekit/src/lib/components/Pill.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	let className = ''
+
+	export {className as class}
+</script>
+
+<span class={`${className} rounded-lg px-2 text-sm font-light text-white`}>
+	<slot />
+</span>
---
+++ apps/sveltekit/src/routes/the-list/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import NaughtyOrNiceTracker from './NaughtyOrNiceTracker.svelte'
+
+	export let data
+</script>
+
+<main class="content-grid gap-y-4 lg:gap-y-8">
+	<h1 class="text-4xl font-bold lg:text-6xl">Naughty or Nice Tracker</h1>
+
+	<NaughtyOrNiceTracker kids={data.kids} />
+</main>
