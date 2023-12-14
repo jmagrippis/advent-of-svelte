95b496d7d80d6fafc6f04461c36d7c3c39fcc1ec
Johnny Magrippis <j@magrippis.com>
2023-12-14 21:16:51 +0100

    ♻️  just set an interval

--- packages/advent-api/src/fetchHeartRate.ts
+++ packages/advent-api/src/fetchHeartRate.ts
@@ -2,9 +2,12 @@ type HeartRateResponse = {
 	heartRate: 65
 }
 
-export const fetchHeartRate = async (): Promise<number> => {
+export const fetchHeartRate = async (
+	signal: AbortSignal | null = null,
+): Promise<number> => {
 	const response = await fetch(
 		'https://advent.sveltesociety.dev/data/2023/day-four.json',
+		{signal},
 	)
 	if (response.ok) {
 		const json = (await response.json()) as HeartRateResponse
