8075dfa547d19bfc69add1876a2fab0bd17e6c3d
Johnny Magrippis <j@magrippis.com>
2023-12-14 21:07:46 +0100

    ✨  stream heart-rates

---
+++ packages/advent-api/src/fetchHeartRate.ts
@@ -0,0 +1,15 @@
+type HeartRateResponse = {
+	heartRate: 65
+}
+
+export const fetchHeartRate = async (): Promise<number> => {
+	const response = await fetch(
+		'https://advent.sveltesociety.dev/data/2023/day-four.json',
+	)
+	if (response.ok) {
+		const json = (await response.json()) as HeartRateResponse
+		return json.heartRate
+	} else {
+		throw Error('Could not fetch heart rate')
+	}
+}
