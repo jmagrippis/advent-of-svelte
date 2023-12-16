86428bebbed003caaec0b59151205ad06aa151e0
Johnny Magrippis <j@magrippis.com>
2023-12-16 20:44:46 +0100

    ✨ add Morse Code Translator

---
+++ apps/sveltekit/src/routes/morse/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import PageTitle from '$lib/components/PageTitle.svelte'
+	import EncoderDecoder from './EncoderDecoder.svelte'
+</script>
+
+<main class="content-grid grow gap-y-4 lg:gap-y-8">
+	<PageTitle>Morse Code Translator</PageTitle>
+
+	<p>
+		Input your message, and see it translated into Morse Code! You may use
+		<strong>letters</strong> (A-Z), <strong>numbers</strong> (0-9) and
+		<strong>spaces</strong> ( ).
+	</p>
+
+	<EncoderDecoder />
+</main>
--- apps/sveltekit/vite.config.ts
+++ apps/sveltekit/vite.config.ts
@@ -1,7 +1,11 @@
+/// <reference types="vitest" />
 import {sveltekit} from '@sveltejs/kit/vite'
 import {enhancedImages} from '@sveltejs/enhanced-img'
 import {defineConfig} from 'vite'
 
 export default defineConfig({
 	plugins: [enhancedImages(), sveltekit()],
+	test: {
+		clearMocks: true,
+	},
 })
