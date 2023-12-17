875041e629776d41c9b322ec0c9ae8831572f25b
Johnny Magrippis <j@magrippis.com>
2023-12-17 21:43:03 +0100

    🐛  we want a full minute / day / month / year to pass before "counting" it

--- apps/sveltekit/src/lib/getTimeDifference.test.ts
+++ apps/sveltekit/src/lib/getTimeDifference.test.ts
@@ -18,23 +18,23 @@ it('returns the amount of months between two dates', () => {
 	const christmas = new Date('2023-12-25')
 	const dateA = new Date('2023-11-24')
 
-	const resultA = getTimeDifference(christmas, dateA)
-	expect(resultA.months).toBe(-1)
+	const resultA = getTimeDifference(dateA, christmas)
+	expect(resultA.months).toBe(1)
 
-	const dateB = new Date('2023-10-24')
+	const dateB = new Date('2023-10-19')
 
-	const resultB = getTimeDifference(christmas, dateB)
-	expect(resultB.months).toBe(-2)
+	const resultB = getTimeDifference(dateB, christmas)
+	expect(resultB.months).toBe(2)
 })
 
 it('returns the amount of days between two dates', () => {
-	const christmas = new Date('2023-12-25')
-	const dateA = new Date('2023-12-17')
+	const christmas = new Date('2023-12-25T00:00:00.000Z')
+	const dateA = new Date('2023-12-17T00:00:00.000Z')
 
 	const resultA = getTimeDifference(christmas, dateA)
 	expect(resultA.days).toBe(-8)
 
-	const dateB = new Date('2023-12-24')
+	const dateB = new Date('2023-12-24T00:00:00.000Z')
 
 	const resultB = getTimeDifference(christmas, dateB)
 	expect(resultB.days).toBe(-1)
@@ -57,14 +57,15 @@ it('can return a combination of days and hours between two dates', () => {
 	const christmas = new Date('2023-12-25T00:00:00.000Z')
 	const dateA = new Date('2023-12-23T20:00:00.000Z')
 
-	const resultA = getTimeDifference(christmas, dateA)
-	expect(resultA.days).toBe(-1)
-	expect(resultA.hours).toBe(-4)
+	const resultA = getTimeDifference(dateA, christmas)
+	expect(resultA.days).toBe(1)
+	expect(resultA.hours).toBe(4)
 
 	const dateB = new Date('2023-12-24T15:00:00.000Z')
 
-	const resultB = getTimeDifference(christmas, dateB)
-	expect(resultB.hours).toBe(-9)
+	const resultB = getTimeDifference(dateB, christmas)
+	expect(resultB.days).toBe(0)
+	expect(resultB.hours).toBe(9)
 })
 
 it('returns the amount of minutes between two dates', () => {
@@ -78,6 +79,19 @@ it('returns the amount of minutes between two dates', () => {
 
 	const resultB = getTimeDifference(christmas, dateB)
 	expect(resultB.minutes).toBe(-10)
+})
+
+it('returns 0 when it has not been a full minute', () => {
+	const christmas = new Date('2023-12-25T00:00:00.000Z')
+	const dateA = new Date('2023-12-24T23:59:40.000Z')
+
+	const resultA = getTimeDifference(dateA, christmas)
+	expect(resultA.minutes).toBe(0)
+
+	const dateB = new Date('2023-12-24T23:59:01.000Z')
+
+	const resultB = getTimeDifference(dateB, christmas)
+	expect(resultB.minutes).toBe(0)
 })
 
 it('returns the amount of seconds between two dates', () => {
